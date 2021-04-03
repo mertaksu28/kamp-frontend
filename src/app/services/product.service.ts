12505b8b19bdc6f73b1993d9d4e386530dc9343e
mertaksu28 <mertaksu28@outlook.com.tr>
2021-04-03 19:02:41 +0300

    error

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { ListResponseModule } from '../modules/listResponseModule';
 import { Product } from '../modules/product';
 import { Category } from '../modules/category';
+import { ResponseModule } from '../modules/ResponseModule';
 @Injectable({
   providedIn: 'root',
 })
@@ -22,5 +23,12 @@ export class ProductService {
     let newPath =
       this.apiUrl + 'products/getbycategory?categoryId=' + categoryId;
     return this.httpClient.get<ListResponseModule<Product>>(newPath);
+  }
+
+  add(product: Product): Observable<ResponseModule> {
+    return this.httpClient.post<ResponseModule>(
+      this.apiUrl + 'products/add',
+      product
+    );
   }
 }
